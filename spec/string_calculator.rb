add1cf513439baa14255c75cc6fceda65748d8a2
lvlnsiva <sivaprasad.n@bitlasoft.com>
2024-07-30 07:51:49 +0530

    remaining spec changes

--- spec/string_calculator.rb
+++ spec/string_calculator.rb
@@ -13,5 +13,35 @@ RSpec.describe StringCalculator, ".add" do
     end
   end
 
- 
+  context "2 numbers" do
+    it "returns 5 for 2,3" do
+      expect(StringCalculator.add("2,3")).to eql(5)
+    end
+  end
+
+  context "3 numbers" do
+    it "returns 6 for 1,2,3" do
+      expect(StringCalculator.add("1,2,3")).to eql(6)
+    end
+  end
+
+  context "many numbers" do
+    it "returns 1000 for 100 10s" do
+      expect(StringCalculator.add("#{(["10"]*100).join(",")}")).to eql(1000)
+    end
+  end
+
+  context "new lines as delimiters" do
+    it "supoorts new lines" do
+      expect(StringCalculator.add("1\n2")).to eql(3)
+    end
+  end
+
+  context "raise error for negitive numbers" do
+    it "raises an exception for negitive numbers" do
+      Proc.new{
+        expect(StringCalculator.add("-1")).to raise_error 
+      }
+    end
+  end
 end
